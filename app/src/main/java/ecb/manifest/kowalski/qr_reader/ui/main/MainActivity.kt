fd5a42d07bd1c923b711d232edbb10dbcf922bc1
Kaue Miziara <kauemiziara@gmail.com>
2023-08-23 18:50:47 -0300

    Move the button handling logic back to the ViewModel

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainActivity.kt
@@ -7,14 +7,11 @@ import androidx.lifecycle.ViewModelProvider
 import ecb.manifest.kowalski.qr_reader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: MainActivityViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
     }
 }
--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainScreenFragment.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainScreenFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import ecb.manifest.kowalski.qr_reader.R
@@ -13,6 +13,7 @@ import ecb.manifest.kowalski.qr_reader.databinding.FragmentMainScreenBinding
 
 class MainScreenFragment : Fragment() {
     private lateinit var binding: FragmentMainScreenBinding
+    private lateinit var viewModel: MainActivityViewModel
     private lateinit var navController: NavController
 
     override fun onCreateView(
@@ -23,21 +24,17 @@ class MainScreenFragment : Fragment() {
         binding = FragmentMainScreenBinding.inflate(inflater, container, false)
         navController = findNavController()
 
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
         setupClickListeners()
-
         return binding.root
     }
 
     private fun setupClickListeners() {
-        setButtonClick(binding.qrCodeButton) {
+        viewModel.setButtonClick(binding.qrCodeButton) {
             navController.navigate(R.id.action_mainScreenFragment_to_qrCodeReaderFragment)
         }
-        setButtonClick(binding.numericCodeButton) {
+        viewModel.setButtonClick(binding.numericCodeButton) {
             navController.navigate(R.id.action_mainScreenFragment_to_numericCodeReaderFragment)
         }
-    }
-
-    private fun setButtonClick(button: Button, clickAction: () -> Unit) {
-        button.setOnClickListener { clickAction.invoke() }
     }
 }
