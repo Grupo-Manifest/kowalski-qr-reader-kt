769c6039c7876eb283d1e8905ffe89fd9d725667
Kaue Miziara <kauemiziara@gmail.com>
2023-08-23 15:04:26 -0300

    Add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,10 @@ dependencies {
 
     // Saved state module for ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+
+    // Navigation
+    val nav_version = "2.5.3"
+
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
