40731f3601637330288a9b3c5dd00461c121599f
Kaue Miziara <kauemiziara@gmail.com>
2023-08-22 22:13:18 -0300

    Remove unused uiState properties and sealed class

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/base/BaseViewModel.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/base/BaseViewModel.kt
@@ -4,9 +4,4 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModel : ViewModel() {
-    protected val _uiState = MutableLiveData<UiState>()
-    val uiState: LiveData<UiState> = _uiState
-
-    sealed class UiState {}
-}
+abstract class BaseViewModel : ViewModel() {}
