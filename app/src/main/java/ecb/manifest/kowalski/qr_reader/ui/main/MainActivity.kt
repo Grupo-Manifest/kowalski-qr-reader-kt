423dd13a2401b2307f3cfae61bc48145e8e81b63
Kaue Miziara <kauemiziara@gmail.com>
2023-08-22 23:51:52 -0300

    Setup bindings connecting the Main View to its ViewModel

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainActivity.kt
@@ -2,16 +2,30 @@ package ecb.manifest.kowalski.qr_reader.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
-import ecb.manifest.kowalski.qr_reader.R
+import ecb.manifest.kowalski.qr_reader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        setButtonClick(binding.qrCodeButton) { viewModel.onQrCodeButtonClick() }
+        setButtonClick(binding.numericCodeButton) { viewModel.onNumericCodeButtonClick() }
+    }
+
+    private fun setButtonClick(button: Button, clickAction: () -> Unit) {
+        button.setOnClickListener { clickAction.invoke() }
     }
 }
--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/main/MainActivityViewModel.kt
@@ -3,5 +3,11 @@ package ecb.manifest.kowalski.qr_reader.ui.main
 import ecb.manifest.kowalski.qr_reader.base.BaseViewModel
 
 class MainActivityViewModel : BaseViewModel() {
-    init {}
+    fun onQrCodeButtonClick() {
+        println("Switching to Qr code screen")
+    }
+
+    fun onNumericCodeButtonClick() {
+        println("Switching to Numeric code screen")
+    }
 }
