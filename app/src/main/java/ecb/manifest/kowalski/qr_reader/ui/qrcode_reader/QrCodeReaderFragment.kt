1997c47d4dff3f908d3585f108389d8f948308ad
Kaue Miziara <kauemiziara@gmail.com>
2023-08-24 01:28:01 -0300

    Implement QrCode scan functionality

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/qrcode_reader/QrCodeReaderFragment.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/qrcode_reader/QrCodeReaderFragment.kt
@@ -1,16 +1,28 @@
 package ecb.manifest.kowalski.qr_reader.ui.qrcode_reader
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 import ecb.manifest.kowalski.qr_reader.R
 import ecb.manifest.kowalski.qr_reader.databinding.FragmentQrCodeReaderBinding
 
 class QrCodeReaderFragment : Fragment() {
     private lateinit var binding: FragmentQrCodeReaderBinding
+
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +36,91 @@ class QrCodeReaderFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+            ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_REQUEST_CODE
+            )
+        } else {
+            startScan()
+        }
+
         binding.qrReturnButton.setOnClickListener {
             findNavController().navigate(R.id.action_qrCodeReaderFragment_to_mainScreenFragment)
         }
+    }
+
+    private fun startScan() {
+        codeScanner = CodeScanner(requireContext(), binding.qrScannerView)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        codeScanner.decodeCallback = DecodeCallback {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), "Scan Result: ${it.text}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        codeScanner.errorCallback = ErrorCallback {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(),
+                    "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        binding.qrScannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireContext(),
+                    "Camera permission granted", Toast.LENGTH_SHORT)
+                    .show()
+
+                startScan()
+            } else {
+                Toast.makeText(requireContext(),
+                    "Camera permission denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::codeScanner.isInitialized) {
+            codeScanner?.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        if (::codeScanner.isInitialized) {
+            codeScanner?.releaseResources()
+        }
+        super.onPause()
+    }
+
+    companion object {
+        private const val CAMERA_REQUEST_CODE = 1
     }
 }
