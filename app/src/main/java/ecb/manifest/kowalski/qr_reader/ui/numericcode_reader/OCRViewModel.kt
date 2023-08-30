7a0c1a5393978f90e4fa33dd6693582e9863b064
Kaue Miziara <kauemiziara@gmail.com>
2023-08-30 19:07:09 -0300

    Extend String class with isNumeric method

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/numericcode_reader/OCRViewModel.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/numericcode_reader/OCRViewModel.kt
@@ -1,9 +1,23 @@
 package ecb.manifest.kowalski.qr_reader.ui.numericcode_reader
 
 import androidx.lifecycle.ViewModel
+import java.lang.Double.parseDouble
+import java.lang.NumberFormatException
 
 class OCRViewModel : ViewModel() {
     fun onTextFound(foundText: String) {
         // TODO
+    }
+
+    private fun String.isNumeric(): Boolean {
+        var numeric = true
+
+        try {
+            val _num = parseDouble(this)
+        } catch (e: NumberFormatException) {
+            numeric = false
+        }
+
+        return numeric
     }
 }
