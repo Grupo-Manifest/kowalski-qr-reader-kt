aa4d0afaf09c4a859b5176c125946c60c7c9b42a
Kaue Miziara <kauemiziara@gmail.com>
2023-08-30 19:03:02 -0300

    Implement OCR text processing backend

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/data/ocr/OCRAnalyzer.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/data/ocr/OCRAnalyzer.kt
@@ -1,12 +1,52 @@
 package ecb.manifest.kowalski.qr_reader.data.ocr
 
+import android.media.Image
+import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.io.IOException
 
 class OCRAnalyzer(
     private val textFoundListener: (String) -> Unit
 ) : ImageAnalysis.Analyzer {
-    override fun analyze(image: ImageProxy) {
-      // TODO
+    @ExperimentalGetImage
+    override fun analyze(imageProxy: ImageProxy) {
+      imageProxy.image?.let { process(it, imageProxy) }
+    }
+
+    private fun process(image: Image, imageProxy: ImageProxy) {
+        try {
+            readTextFromImage(
+                InputImage.fromMediaImage(image, 90),
+                imageProxy,
+                )
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun readTextFromImage(image: InputImage, imageProxy: ImageProxy) {
+        TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+            .process(image)
+            .addOnSuccessListener { visionText ->
+                processTextFromImage(visionText, imageProxy)
+                imageProxy.close()
+            }
+            .addOnFailureListener { error ->
+                error.printStackTrace()
+                imageProxy.close()
+            }
+    }
+
+    private fun processTextFromImage(visionText: Text, imageProxy: ImageProxy) {
+        for (block in visionText.textBlocks) {
+            for (line in block.text) {
+                textFoundListener(line.toString())
+            }
+        }
     }
 }
