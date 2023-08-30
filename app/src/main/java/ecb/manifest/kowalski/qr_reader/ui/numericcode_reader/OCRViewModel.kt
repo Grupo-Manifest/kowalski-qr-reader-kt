58c6f3093fd06f3c2586277fa41bf97a3a8f75ac
Kaue Miziara <kauemiziara@gmail.com>
2023-08-30 19:12:13 -0300

    Implement onTextFound method in the ViewModel

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/numericcode_reader/OCRViewModel.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/numericcode_reader/OCRViewModel.kt
@@ -1,19 +1,21 @@
 package ecb.manifest.kowalski.qr_reader.ui.numericcode_reader
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import java.lang.Double.parseDouble
 import java.lang.NumberFormatException
 
 class OCRViewModel : ViewModel() {
-    fun onTextFound(foundText: String) {
-        // TODO
+    fun onTextFound(foundText: String): String? {
+        return if (foundText.isNumeric()) foundText
+        else null
     }
 
     private fun String.isNumeric(): Boolean {
         var numeric = true
 
         try {
-            val _num = parseDouble(this)
+            parseDouble(this)
         } catch (e: NumberFormatException) {
             numeric = false
         }
