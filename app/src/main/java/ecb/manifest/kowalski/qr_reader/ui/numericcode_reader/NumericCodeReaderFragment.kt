a8dde998f8f453b7555adc0f082874399789ff53
Kaue Miziara <kauemiziara@gmail.com>
2023-08-30 18:14:37 -0300

    Implement startCamera method

--- app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/numericcode_reader/NumericCodeReaderFragment.kt
+++ app/src/main/java/ecb/manifest/kowalski/qr_reader/ui/numericcode_reader/NumericCodeReaderFragment.kt
@@ -3,15 +3,20 @@ package ecb.manifest.kowalski.qr_reader.ui.numericcode_reader
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ecb.manifest.kowalski.qr_reader.R
 import ecb.manifest.kowalski.qr_reader.databinding.FragmentNumericCodeReaderBinding
+import java.lang.IllegalStateException
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -35,7 +40,7 @@ class NumericCodeReaderFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if (allPermissionsGranted()) {
-            // TODO: start camera
+            startCamera()
         } else {
             requestPermissions()
         }
@@ -48,6 +53,21 @@ class NumericCodeReaderFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         cameraExecutor.shutdown()
+    }
+
+    private fun startCamera() {
+        val cameraProvider = ProcessCameraProvider.getInstance(requireContext())
+        cameraProvider.addListener(
+            Runnable {
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(binding.ocrCameraPreview.surfaceProvider)
+                    }
+                cameraProvider.get().bind(preview)
+            },
+            ContextCompat.getMainExecutor(requireContext())
+        )
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -63,7 +83,21 @@ class NumericCodeReaderFragment : Fragment() {
         )
     }
 
+    private fun ProcessCameraProvider.bind(
+        preview: Preview,
+    ) = try {
+        unbindAll()
+        bindToLifecycle(
+            requireActivity(),
+            CameraSelector.DEFAULT_BACK_CAMERA,
+            preview,
+        )
+    } catch (e: IllegalStateException) {
+        Log.e(TAG, "Binding failed", e)
+    }
+
     private companion object {
+        val TAG = NumericCodeReaderFragment::class.java.simpleName
         const val REQUEST_CODE_PERMISSIONS = 10
         val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
